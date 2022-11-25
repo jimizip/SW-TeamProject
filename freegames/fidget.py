002a40d7c93952cc6d395d932c22d16f306afe7c
Kyunghyun1121 <90241597+Kyunghyun1121@users.noreply.github.com>
2022-11-26 00:26:00 +0900

    Update fidget.py

--- freegames/fidget.py
+++ freegames/fidget.py
@@ -15,36 +15,40 @@ state = {'turn': 0}
 
 def spinner():
     """Draw fidget spinner."""
-    clear()
-    angle = state['turn'] / 10
-    right(angle)
-    forward(100)
-    dot(120, 'red')
-    back(100)
+    clear() #turtle을 초기화
+    angle = state['turn'] / 10  #turn의 값을 10으로 나눈 수만큼 angle에 할당
+    
+    right(angle)    #angle의 각도 만큼 이동
+    forward(100)    #100px 만큼 이동시켜 피젯의 뼈대를 선을 그리고
+    dot(120, 'red') #거북이의 좌표에 빨간색 120크기의 점을 만든 후
+    back(100)   #back으로 다시 뒤로 옮깁니다. 
+    #이런방식으로 파란색, 초록생의 원을 만들고 turtle 스크린을 update를 사용해 갱신합니다.
     right(120)
     forward(100)
     dot(120, 'green')
     back(100)
+
     right(120)
     forward(100)
     dot(120, 'blue')
     back(100)
+
     right(120)
     update()
 
 
-def animate():
+def animate():  #피젯스피너를 돌리는 함수
     """Animate fidget spinner."""
-    if state['turn'] > 0:
+    if state['turn'] > 0:   #turn의 값이 0보다 크면 1을 감소시키고 
         state['turn'] -= 1
 
     spinner()
-    ontimer(animate, 20)
+    ontimer(animate, 20)    #spinner를 호출 한 뒤 20밀리초마다 animate를 실행합니다.
 
 
 def flick():
     """Flick fidget spinner."""
-    state['turn'] += 10
+    state['turn'] += 10 #변수 state의 turn을 10 증가시킵니다.
 
 
 setup(420, 420, 370, 0)
@@ -55,3 +59,8 @@ onkey(flick, 'space')
 listen()
 animate()
 done()
+
+# 피젯의 형태가 정해져 있고 ui가 단조롭습니다.
+#  피젯을 다른 모양으로도 만들어서 
+# 사용자가 선택하여 게임을 더욱 다채롭게 개선할 예정입니다.
+
