c836ae6f5fca6767640312bd12ac76cccc6532cf
Kyunghyun1121 <90241597+Kyunghyun1121@users.noreply.github.com>
2022-11-26 00:25:11 +0900

    Update ant.py

--- freegames/ant.py
+++ freegames/ant.py
@@ -17,25 +17,25 @@ ant = vector(0, 0)
 aim = vector(2, 0)
 
 
-def wrap(value):
+def wrap(value):        # -200에서 200까지의 값을 래핑해주는 함수
     """Wrap value around -200 and 200."""
     return value  # TODO
 
 
 def draw():
     """Move ant and draw screen."""
-    ant.move(aim)
+    ant.move(aim)    #ant의 좌표에 aim의 좌표를 move를 사용해 넘겨준후
     ant.x = wrap(ant.x)
-    ant.y = wrap(ant.y)
+    ant.y = wrap(ant.y) # wrap을 이용해 깊은 복사를 진행합니다.
 
     aim.move(random() - 0.5)
-    aim.rotate(random() * 10 - 5)
+    aim.rotate(random() * 10 - 5)    #aim을 move와 rotate를 사용해 랜덤하게 좌표를 변환
 
-    clear()
-    goto(ant.x, ant.y)
-    dot(4)
+    clear() #clear로 거북이의 상태를 초기화
+    goto(ant.x, ant.y)  #goto로 ant의 좌표로 이동
+    dot(4) #점 4개를 만듬
 
-    ontimer(draw, 100)
+    ontimer(draw, 100)  #100밀리초에 한번씩 draw를 호출 해줍니다.
 
 
 setup(420, 420, 370, 0)
@@ -44,3 +44,9 @@ tracer(False)
 up()
 draw()
 done()
+
+# 개미의 움직임을 조작하는 것이 아니라 
+# 관찰만 하는것이라 사용자의 흥미를 얻기 힘들것이라 예상되어 개미를 
+# 관찰하는 것 말고도 사용자가 개미의 움직임을 조작할 수 있게 하거나 
+# 개미의 속도를 조절하는 등의 다양한 게임 진행 방법을 추가하여 
+# 사용자의 지속적인 흥미를 유발하도록 개선할 예정입니다.
